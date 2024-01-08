43a39ffc61c6aaf3271accfc6e35d6399805f732
kseoh2 <seoh2k@gmail.com>
2024-01-08 14:35:09 +0900

    스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,15 +9,21 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 스프링 기반으로 변경
+@Configuration // 설정 구성
 public class AppConfig {
 
+    @Bean // 스프링 컨테이너에 스프링 빈(스프링컨테이너에 등록된 객체)으로 등록
     public MemberService memberService() {
         // 리팩터링
         // return new MemberServiceImpl(new MemoryMemberRepository());
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         // 리팩터링
         /** return new OrderServiceImpl(
@@ -30,10 +36,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // 할인 정책 변경
         // return new FixDiscountPolicy();
