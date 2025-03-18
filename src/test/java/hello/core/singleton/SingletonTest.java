11ea5b7b82f6f69eead13bcb6212e641a4027df3
seoh2k <seoh2k@gmail.com>
2025-03-19 05:51:25 +0900

    ...

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,7 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SingletonTeset {
+public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
