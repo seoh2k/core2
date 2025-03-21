bc54c8b7c8bca3aeea97cb3647cc11891550b56e
seoh2k <seoh2k@gmail.com>
2025-03-22 08:06:03 +0900

    컴포넌트 스캔 - 중복 등록과 충돌  - 수동 등록 빈이 우선권을 갖음 -> 하지만 그러면 정말 잡기 어려운 버그가 되기 때문에 최근 스프링 부트에서는 오류가 발생하도록 기본 값을 바꿈

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Component가 붙은 클래스들을 스캔해서 스프링 빈으로 등록
 public class AutoAppConfig {
+
+    // 수동 빈 등록이 자동등록 빈의 이름과 같을 경우
+    /*@Bean(name = "MemoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }*/
 }
