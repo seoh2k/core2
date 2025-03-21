d0476518513b43fdef35bbfc0986ee1c6044fffa
seoh2k <seoh2k@gmail.com>
2025-03-22 07:45:17 +0900

    컴포넌트 스캔 - 탐색 위치와 기본 스캔 대상  - 설정 정보 클래스의 위치를 프로젝트의 최상단에 두는게 최적의 설정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         // @Configuration이 붙은 AppConfig.class는 제외하고 주입
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Component가 붙은 클래스들을 스캔해서 스프링 빈으로 등록
