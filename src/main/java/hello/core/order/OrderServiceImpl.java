1a9a722241a1a52f68afaacd37dd8951d2795cbd
seoh2k <seoh2k@gmail.com>
2025-03-23 08:38:25 +0900

    의존관계 자동주입 - 다양한 의존관계 주입 방법  - 생성자 주입:  불변, 필수 일때 사용  private final 은 값을 넣어야 함  생성자가 하나일 땐 @Autowired 붙이지 않아도 됨
     - 수정자 주입:
     선택, 변경 가능성이 있는 의존관계에서 사용
    
     - 필드 주입:
     DI프레임워크가 없다면 아무것도 할 수 없음
     Test code에서만 사용
    
     - 일반 메서드 주입
     잘 사용 안함

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,15 +9,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
+    // 생성자 주입(final이 붙으면 값을 넣어야 함)
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
     // 인터페이스에만 의존하도록 코드 변경
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private DiscountPolicy discountPolicy;
 
+    // 수정자 주입: 생성자 주입이 필요 없음
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired // 생성자가 하나일때는 자동으로 적용된다
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
